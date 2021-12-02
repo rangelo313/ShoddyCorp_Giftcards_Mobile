25421f30b3d725d82b3fc8d3f84302ecc3a6eee2
Rob <rac16c@my.fsu.edu>
2021-12-02 12:43:05 -0500

    part_3_complete

---
+++ part3/CardScrollingActivity.kt
@@ -0,0 +1,101 @@
+package com.example.giftcardsite
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.giftcardsite.api.model.*
+import com.example.giftcardsite.api.service.CardInterface
+import com.example.giftcardsite.api.service.UserInfo
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class CardScrollingActivity : AppCompatActivity() {
+    private var loggedInUser : User? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val locationPermissionCode = 2
+
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+       // locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+
+
+        setContentView(R.layout.activity_scrolling)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        loggedInUser = intent.getParcelableExtra<User>("User")
+        var button : Button = findViewById<Button>(R.id.view_cards_button)
+        button.text = "View Products"
+        button.setOnClickListener {
+            val intent = Intent(this, ProductScrollingActivity::class.java).apply{
+                putExtra("User", loggedInUser)
+            }
+            startActivity(intent)
+        }
+        var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsecclass.report").addConverterFactory(
+            GsonConverterFactory.create())
+        var retrofit: Retrofit = builder.build()
+        var client: CardInterface = retrofit.create(CardInterface::class.java)
+        val outerContext = this
+        var manager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val token = "Token ${loggedInUser?.token}"
+        recyclerView.layoutManager = manager
+        client.getCards(token)?.enqueue(object :
+            Callback<List<Card?>?> {
+            override fun onFailure(call: Call<List<Card?>?>, t: Throwable) {
+                Log.d("Product Failure", "Product Failure in onFailure")
+                Log.d("Product Failure", t.message.toString())
+
+            }
+
+            override fun onResponse(call: Call<List<Card?>?>, response: Response<List<Card?>?>) {
+                if (!response.isSuccessful) {
+                    Log.d("Product Failure", "Product failure. Yay.")
+                }
+                var cardListInternal = response.body()
+                Log.d("Product Success", "Product Success. Boo.")
+                if (cardListInternal == null) {
+                    Log.d("Product Failure", "Parsed null product list")
+                    Log.d("Product Failure", response.toString())
+                } else {
+                    recyclerView.adapter = CardRecyclerViewAdapter(outerContext, cardListInternal, loggedInUser)
+                }
+            }
+        })
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ part3/ProductScrollingActivity.kt
@@ -0,0 +1,101 @@
+package com.example.giftcardsite
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.Button
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.giftcardsite.api.model.*
+import com.example.giftcardsite.api.service.ProductInterface
+import com.example.giftcardsite.api.service.UserInfo
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ProductScrollingActivity : AppCompatActivity(){
+    var loggedInUser: User? = null
+
+    private var lastEvent : String? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val locationPermissionCode = 2
+
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+
+        setContentView(R.layout.activity_scrolling)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+        loggedInUser = intent.getParcelableExtra<User>("User")
+        findViewById<Button>(R.id.view_cards_button).setOnClickListener{
+            val intent = Intent(this, CardScrollingActivity::class.java).apply {
+                putExtra("User", loggedInUser)
+            }
+            startActivity(intent)
+        }
+        //var productList: List<Product?>? = null
+        var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsecclass.report").addConverterFactory(
+                GsonConverterFactory.create())
+        var retrofit: Retrofit = builder.build()
+        var client: ProductInterface = retrofit.create(ProductInterface::class.java)
+        val outerContext = this
+        var manager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = manager
+        client.getAllProducts()?.enqueue(object :
+                Callback<List<Product?>?> {
+            override fun onFailure(call: Call<List<Product?>?>, t: Throwable) {
+                Log.d("Product Failure", "Product Failure in onFailure")
+                Log.d("Product Failure", t.message.toString())
+
+            }
+
+            override fun onResponse(call: Call<List<Product?>?>, response: Response<List<Product?>?>) {
+                if (!response.isSuccessful) {
+                    Log.d("Product Failure", "Product failure. Yay.")
+                }
+                var productListInternal = response.body()
+                Log.d("Product Success", "Product Success. Boo.")
+                if (productListInternal == null) {
+                    Log.d("Product Failure", "Parsed null product list")
+                    Log.d("Product Failure", response.toString())
+                } else {
+                    recyclerView.adapter = RecyclerViewAdapter(outerContext, productListInternal, loggedInUser)
+                }
+            }
+        })
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+   // override fun onLocationChanged(location: Location) {
+    //    var userInfoContainer = UserInfoContainer(location, null, loggedInUser?.token)
+    //    var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsecclass.report").addConverterFactory(
+    //        GsonConverterFactory.create())
+     //   var retrofit: Retrofit = builder.build()
+    //    var client: UserInfo = retrofit.create(UserInfo::class.java)
+
+
+
+
+
+
+}
